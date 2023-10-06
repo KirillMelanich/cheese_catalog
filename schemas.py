11284c1ac7a87fee1cc611c541d20a02388e27a1
KirillMelanich <kirillmelanich@gmail.com>
2023-10-06 15:11:43 +0300

    Add schemas.py and crud

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class CheeseTypeBase(BaseModel):
+    name: str
+    description: str
+
+
+class CheeseTypeCreate(CheeseTypeBase):
+    pass
+
+
+class CheeseType(CheeseTypeBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
